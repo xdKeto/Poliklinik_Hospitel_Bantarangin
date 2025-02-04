e03cf6ca54095b681be81e7b27b67345a329a6d3
Yestoya L Minggus <yestoya.minggus@gmail.com>
2025-02-04 14:29:16 +0700

    login screen UI

---
+++ Frontend-Flutter/poli_admin/lib/screens/list_pasien/list_pasien_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ListPasienScreen extends StatefulWidget {
+  const ListPasienScreen({super.key});
+
+  @override
+  State<ListPasienScreen> createState() => _ListPasienScreenState();
+}
+
+class _ListPasienScreenState extends State<ListPasienScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'ini list pasien'
+      ),
+    );
+  }
+}

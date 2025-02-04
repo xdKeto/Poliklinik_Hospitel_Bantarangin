09ce1a5e7528b5226ba8696e8719b90e791bd11f
Yestoya L Minggus <yestoya.minggus@gmail.com>
2025-02-04 10:00:57 +0700

    asaaaaa

---
+++ Frontend-Flutter/poli_admin/lib/base/utils/app_styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static Color primaryColor = const Color(0xff45496A);
+  static Color secondaryColor = const Color(0xff9AC5E5);
+  static Color accentColor = const Color(0xffF6C445);
+  static Color backgroundColor = const Color(0xffF4F4F4);
+  static Color textColor = const Color(0xff171717);
+  static Color redColor = const Color(0xffD31638);
+  static Color greenColor = const Color(0xff59FC3C);
+  static Color greyColor = const Color(0xffE3E3E3);
+  static Color greyColor2 = const Color(0xffA9A9A9);
+  static Color inputBox = const Color(0xffF6F6F6);
+
+  static TextStyle normalFont = TextStyle(
+    fontWeight: FontWeight.bold,
+  );
+}

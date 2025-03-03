ebeddc0cacbc56535019c70c1f742d6512bce02b
Yestoya L Minggus <yestoya.minggus@gmail.com>
2025-03-03 10:13:24 +0700

    fix rincian scroll

--- Frontend-Flutter/poli_suster/lib/screens/rincian/data_kesehatan.dart
+++ Frontend-Flutter/poli_suster/lib/screens/rincian/data_kesehatan.dart
@@ -8,43 +8,41 @@ class DataKesehatan extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      // height: 300,
       width: double.infinity,
       decoration: AppStyles.whiteBox,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              DataField(title: "Keluhan Utama", data: "Keluhan Utama"),
-              SizedBox(
-                height: 16,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: DataField(
-                        title: "Riwayat Penyakit", data: "Riwayat Penyakit"),
-                  ),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  Expanded(child: DataField(title: "Alergi", data: "Alergi")),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  Expanded(
-                      child: DataField(
-                          title: "Jenis Reaksi", data: "Jenis Reaksi")),
-                ],
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              DataField(
-                  title: "Keluhan Umum Pasien", data: "Keluhan Umum Pasien"),
-            ],
-          ),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            DataField(title: "Keluhan Utama", data: "Keluhan Utama"),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: DataField(
+                      title: "Riwayat Penyakit", data: "Riwayat Penyakit"),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(child: DataField(title: "Alergi", data: "Alergi")),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                    child:
+                        DataField(title: "Jenis Reaksi", data: "Jenis Reaksi")),
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            DataField(
+                title: "Keluhan Umum Pasien", data: "Keluhan Umum Pasien"),
+          ],
         ),
       ),
     );
